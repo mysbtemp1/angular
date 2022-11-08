221eb7561ee0b58c2e5c91bc9d7bd2525d33e623
Siddhid Gopujkar <64736645+SiddhidGopujkar@users.noreply.github.com>
2022-11-08 17:03:24 +0530

    WIP post request

--- src/app/components/crud/crud.component.ts
+++ src/app/components/crud/crud.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ImagesComponent } from '../layouts/partials/images/images.component';
 
+import { ServerService } from '../../services/server.service';
+import { Image } from '../../models/image.model';
+
 @Component({
   selector: 'app-crud',
   templateUrl: './crud.component.html',
@@ -9,7 +12,7 @@ export class CrudComponent implements OnInit {
 
   new_image_path:string = '';
 
-  constructor() { }
+  constructor(private service: ServerService) { }
 
   ngOnInit(): void {
   }
@@ -17,8 +20,9 @@ export class CrudComponent implements OnInit {
   test()
   {
     console.log(this.new_image_path);
-    this.new_image_path = '';
     // new ImagesComponent.pushImage(new_image_path);
+    this.service.saveImage(this.new_image_path);
+    // this.new_image_path = '';
   }
 
 }
--- src/app/services/server.service.ts
+++ src/app/services/server.service.ts
@@ -2,17 +2,24 @@ import { Injectable } from '@angular/core';
 
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Image } from '../models/image.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ServerService {
   
-  private url = 'http://localhost:8000/api/images';
+  private get_images_url = 'http://localhost:8000/api/images';
+  private post_image_url = 'http://localhost:8000/api/image/save';
 
   constructor(private http: HttpClient) { }
 
   getImages(){
-    return this.http.get(this.url);
+    return this.http.get(this.get_images_url);
+  }
+
+  saveImage(data: String){
+    console.log('in post' + data);
+    return this.http.post(this.post_image_url, data);
   }
 }
