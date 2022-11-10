d7a376abead085870f420647aa258485fb988f06
Siddhid Gopujkar <64736645+SiddhidGopujkar@users.noreply.github.com>
2022-11-10 13:50:33 +0530

    authguard done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomeComponent } from './components/home/home.component';
 import { CrudComponent } from './components/crud/crud.component';
+import { AuthGuard } from './shared/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'manage_images', component: CrudComponent }
+  { path: 'manage_images', component: CrudComponent, canActivate: [AuthGuard] }
 ];
 
 @NgModule({
